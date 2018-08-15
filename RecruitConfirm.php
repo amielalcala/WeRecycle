037ca9d7dd84eee24210c28f999faa045f6c9812
Francis Norman Macabunga <francisnormanmacabunga@gmail.com>
2018-08-16 03:14:07 +0800

    Create RecruitConfirm.php

---
+++ RecruitConfirm.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <h2>Thank you for your interest</h2>
+    <h4>Contact Activity Coordinator to confirm your attendance!
+      <br> Message/Call: 123456789!
+    </h4>
+  </head>
+  <body>
+  </body>
+</html>
